6c137c6e1d0f192665a1210bed951cc48d9cebff
Alexandre Marques <xan372016@gmail.com>
2026-02-22 18:46:15 -0300

    chore: sincroniza projeto com o estado atual do curso (Prisma 4.13.0)

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,12 @@
-import 'dotenv/config';
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
-
-const app = express();
 const port = 3000;
+const app = express();
 
-app.use(express.json());
-
-app.get('/movies', async (req, res) => {
-  try {
-    const movies = await prisma.movies.findMany({
-      include: {
-        genres: true,
-        languages: true
-      }
-    });
-    res.json(movies);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro ao buscar filmes' });
-  }
+app.get('/movies', (req, res) => {
+  res.send('  Listagem de Filmes!');
 });
 
 app.listen(port, () => {
-  console.log(`Servidor em execução na porta ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
